bcee59393b19e66f844527575f33a4eaa9e57d28
epaschke22 <ericksonpaschke@gmail.com>
2021-03-30 20:15:40 +0000

    updating object files for task 7 8 and 9 updated city, place, user, and review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel):
     """ Review classto store review information """
     place_id = ""
     user_id = ""
-    text = ""
+    text = Column(String(1024), nullable=False)
