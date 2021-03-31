e9b8c307e5ea4c9ebb7ba29cac47d7401018b458
epaschke22 <ericksonpaschke@gmail.com>
2021-03-31 18:27:02 +0000

    updated place to set the table properly and new init method in base model

--- models/place.py
+++ models/place.py
@@ -6,11 +6,13 @@ from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
-place_amenity = Table(metadata=Base.metadata,
+place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
-                             ForeignKey('place.id')),
+                             ForeignKey('places.id'), 
+                             primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
-                             ForeignKey('amenity.id')))
+                             ForeignKey('amenities.id'), 
+                             primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
