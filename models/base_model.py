911b38d7ce452932d652f937b339a24cd8b7d87a
epaschke22 <ericksonpaschke@gmail.com>
2021-03-31 19:03:50 +0000

    fixed kwargs in init method for base model

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,8 @@ class BaseModel:
                             (value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, value)
+            if 'id' not in kwargs or self.id is None:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
