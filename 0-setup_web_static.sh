036815fa07c14d9b20e7bca995ebd36fb3ebac67
epaschke22 <ericksonpaschke@gmail.com>
2021-04-15 16:19:50 +0000

    submitting task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# sets up web server for deployment
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo printf '%s\n' \
+'<html>' \
+'  <head>' \
+'  </head>' \
+'  <body>' \
+'    Holberton School' \
+'  </body>' \
+'</html>' \
+>> /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+new_string="location /hbnb_static { alias /data/web_static/current/; }"
+sudo sed -i "/listen \[::\]:80 default_server;/a $new_string" /etc/nginx/sites-enabled/default
+service nginx restart
