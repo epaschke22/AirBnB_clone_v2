ec505e3bde02340cf87ac190ce93af7282bde0c9
epaschke22 <ericksonpaschke@gmail.com>
2021-04-15 20:21:56 +0000

    updating task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # sets up web server for deployment
-sudo apt-get -y update
-sudo apt-get -y install nginx
+dpkg -s nginx &> /dev/null
+if [ $? -ne 0 ]; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 sudo printf '%s\n' \
@@ -13,7 +16,8 @@ sudo printf '%s\n' \
 '  </body>' \
 '</html>' \
 | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 new_string="location /hbnb_static {alias /data/web_static/current/;}"
 sudo sed -i "/listen \[::\]:80 default_server;/a $new_string" /etc/nginx/sites-enabled/default
