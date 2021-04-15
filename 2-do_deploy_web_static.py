63c7274eebfd092af9737906d6e23e3d365a970c
epaschke22 <ericksonpaschke@gmail.com>
2021-04-15 19:24:31 +0000

    updating task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 """Deploy archive! """
 from fabric.api import put, run, env
-from fabric.contrib import files
+import os
 env.hosts = ["34.75.67.199", "35.237.13.196"]
+
 
 def do_deploy(archive_path):
     """deploys archaives to the servers"""
-    if !files.exists(archive_path):
+    if os.path.exists(archive_path) == False:
         return False
-    archfile = archive_path.split('/')[-1]
-    arch = archfile.split('.')[0]
-    put(archive_path, "/tmp/{}".format(archfile))
-    run("mkdir -p /data/web_static/releases/{}/".format(arch))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(archfile, arch))
-    run("rm /tmp/{}".format(archfile))
-    run("mv /data/web_static/releases/{}/web_static/* \
-        /data/web_static/releases/{}/".format(arch, arch))
-    run("rm -rf /data/web_static/releases/{}/web_static".format(arch))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(arch))
+    try:
+        archfile = archive_path.split('/')[-1]
+        arch = archfile.split('.')[0]
+        put(archive_path, "/tmp/{}".format(archfile))
+        run("mkdir -p /data/web_static/releases/{}/".format(arch))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archfile, arch))
+        run("rm /tmp/{}".format(archfile))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(arch, arch))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(arch))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(arch))
+        return True
+    except Exception as e:
+        print(e)
+        return False
