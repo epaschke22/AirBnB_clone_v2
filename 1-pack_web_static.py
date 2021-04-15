fb94bdb95f9aacc7424a2870ace0f66ac22ab521
epaschke22 <ericksonpaschke@gmail.com>
2021-04-15 18:07:15 +0000

    submitting task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Compress before sending"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """compresses the web static folder into an archive"""
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    local("tar -cvzf versions/web_static_{}.tgz web_static".format(now))
